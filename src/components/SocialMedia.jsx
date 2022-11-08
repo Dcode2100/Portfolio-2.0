25d832ab8b1d717726b7d051eaac2d7f9723fdfc
Dikshant Ashok Solanki <dcode2100@gmail.com>
2022-11-08 10:55:36 +0530

    BuildErrors

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -5,12 +5,12 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <a href = "https://github.com/dikshant2142001" target="_blank" rel = "noferrer">
+      <a href = "https://github.com/dikshant2142001" target="_blank" rel = "noopener noreferrer">
         <FaGithub />
         </a>
     </div>
     <div>
-    <a href = "https://www.linkedin.com/in/dikshant-solanki-025468150?original_referer=" target="_blank" rel = "noferrer">
+    <a href = "https://www.linkedin.com/in/dikshant-solanki-025468150?original_referer=" target="_blank" rel = "noopener noreferrer">
         <FaLinkedin />
         </a>
     </div>
