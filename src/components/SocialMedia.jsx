4efeac2f13a4fae43f16b2272d44df3610f69593
Dikshant Ashok Solanki <dcode2100@gmail.com>
2022-11-08 10:03:33 +0530

    Done

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <a href = "https://github.com/dikshant2142001" target="_blank" rel = "noferrer">
+        <FaGithub />
+        </a>
+    </div>
+    <div>
+    <a href = "https://www.linkedin.com/in/dikshant-solanki-025468150?original_referer=" target="_blank" rel = "noferrer">
+        <FaLinkedin />
+        </a>
+    </div>
+  </div>
+);
+
+export default SocialMedia;
